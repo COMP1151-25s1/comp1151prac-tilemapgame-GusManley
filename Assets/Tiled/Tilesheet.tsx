961b6c865bbd43451f604f826c2e85cc8dfeea4c
GusManley <manleyangus17@gmail.com>
2025-06-05 21:06:24 +1000

    fixed up tilesheet collision and added hazard layers

--- Assets/Tiled/Tilesheet.tsx
+++ Assets/Tiled/Tilesheet.tsx
@@ -152,6 +152,15 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="61">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="Hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
  <tile id="64">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1" y="1" width="14" height="14"/>
